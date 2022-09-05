9b0193ace974e425df6c4b9de47a6259b2d27ea7
BoboFrank <anozie201040@gmail.com>
2022-09-05 17:59:25 +0100

    fixed errors

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 
 /**
@@ -7,8 +6,3 @@
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
-=======
-#!/usr/bin/node
-const threeLines = "C is fun \nPython is fun \nJavascript is amazing";
-console.log(threeLines);
->>>>>>> d5ed679c6fcaec288c74f7142b965fed1931603e
